d70dcf42df64119d9ce9937772cdd252c64014b8
popov <popich2005@mail.ru>
2024-04-19 13:49:14 +0300

    Initial Commit

---
+++ ColorSettingsSUI/ColorSettingsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSettingsSUIApp.swift
+//  ColorSettingsSUI
+//
+//  Created by Алексей Попов on 19.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorSettingsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
