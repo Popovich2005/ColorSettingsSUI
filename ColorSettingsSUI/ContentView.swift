274024610a3c13c73e4973ee437c9e4987178c4b
popov <popich2005@mail.ru>
2024-04-19 15:15:11 +0300

    version without text field

--- ColorSettingsSUI/ContentView.swift
+++ ColorSettingsSUI/ContentView.swift
@@ -8,17 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValueRed = Double.random(in: 0...255)
+    @State private var sliderValueGreen = Double.random(in: 0...255)
+    @State private var sliderValueBlue = Double.random(in: 0...255)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.cyan
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                ColorView(sliderValueRed: $sliderValueRed,
+                          sliderValueGreen: $sliderValueGreen,
+                          sliderValueBlue: $sliderValueBlue
+                )
+                
+                ColorSliderView(sliderValue: $sliderValueRed)
+                    .tint(.red)
+                ColorSliderView(sliderValue: $sliderValueGreen)
+                    .tint(.green)
+                ColorSliderView(sliderValue: $sliderValueBlue)
+                    .tint(.blue)
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ColorSliderView: View {
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        HStack {
+            Text(lround(sliderValue).formatted()).foregroundStyle(.white)
+            Slider(value: $sliderValue, in: 0...255, step: 1)
+
+        }
+    }
+}
+
+struct ColorView: View {
+    @Binding var sliderValueRed: Double
+    @Binding var sliderValueGreen: Double
+    @Binding var sliderValueBlue: Double
+    
+    var body: some View {
+        Rectangle()
+            .frame(width: 300, height: 200)
+            .foregroundStyle(Color(red: sliderValueRed / 255,
+                                   green: sliderValueGreen / 255,
+                                   blue: sliderValueBlue / 255)
+            )
+            .clipShape(.rect(cornerRadius: 15))
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(.white, lineWidth: 4)
+            )
+    }
 }
